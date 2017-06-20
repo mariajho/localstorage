0dc306b5a06777ff710f68e8b5046bead521867c
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-06-20 15:38:09 -0400

    termino-guiado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,15 @@
 /*desarrollamos una funcion guardarDatos, para recuperar los dato que el usuario ingrese en los input*/
 
-function guardarDatos{
-	localStorage.nombre = document.getElementById("nombre").value;
-	localStorage.password = document.getElementById("password").value;
+function guardarDatos() {
+    localStorage.nombre = document.getElementById("nombre").value;
+    localStorage.password = document.getElementById("password").value;
 }
 
-/*funcion que recupera datos*/
-
-function recuperarDatos(){
-	if ((localStorage.nombre != undefined) && (localStorage.password != undefined)){
-		document.getElementById("datos").inner.HTML = "Nombre: " + localStorage.nombre + "<br/> Password: " + localStorage.password;
-	}
-	else{  
-		document.getElementById("datos").innerHTML = "No has introducido tu nombre y tu password";
-}
+function recuperarDatos() {
+    if ((localStorage.nombre != "") && (localStorage.password != "")) {
+        document.getElementById("datos").innerHTML = "Nombre: " + localStorage.nombre + "<br> Password: " + localStorage.password;
+    }
+    else {
+        document.getElementById("datos").innerHTML = "No has introducido tu nombre y tu password";
+    }
 }
